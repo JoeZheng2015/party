10d922a7af8c403ed27992d7f8f67b6bba65abe7
郑俊鑫 <18810544291@163.com>
2017-04-21 11:06:23 +0800

    bugfix

--- libs/sdk/index.js
+++ libs/sdk/index.js
@@ -2,7 +2,6 @@ var constants = require('./lib/constants');
 var login = require('./lib/login');
 var Session = require('./lib/session');
 var request = require('./lib/request');
-var Tunnel = require('./lib/tunnel');
 
 var exports = module.exports = {
     login: login.login,
@@ -13,8 +12,6 @@ var exports = module.exports = {
 
     request: request.request,
     RequestError: request.RequestError,
-
-    Tunnel: Tunnel,
 };
 
 // 导出错误类型码
