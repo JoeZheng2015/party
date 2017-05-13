78d3d014f1c0478e3678fd8076ad4be94127ecb4
郑俊鑫 <18810544291@163.com>
2017-05-13 14:38:15 +0800

    add eslint to format code style

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-console": 0
+    },
+    "globals": {
+        "wx": true,
+        "App": false,
+        "Page": false,
+        "getApp": false,
+    }
+};
