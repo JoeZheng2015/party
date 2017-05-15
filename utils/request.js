0e4770df0c64ee1e0312688f03225266b45c622b
郑俊鑫 <18810544291@163.com>
2017-05-15 15:38:16 +0800

    update wx-promise-request

--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,9 @@
 import qcloud from '../libs/sdk/index'
-import {request, setRequestFunc} from './wx-promise-request'
+import {request, setConfig} from '../libs/wx-promise-request'
 
-setRequestFunc(qcloud.request)
+setConfig({
+    request: qcloud.request,
+})
 
 export const GET = request('GET')
 export const POST = request('POST')
